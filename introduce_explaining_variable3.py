597f7198f5f7436a2061df910b391a7c3976812c
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-02-01 12:48:22 -0800

    exercise4

--- introduce_explaining_variable3.py
+++ introduce_explaining_variable3.py
@@ -5,8 +5,12 @@ MEDIUM = 600000
 COOKED_CONSTANT = 0.05
 
 def is_cookeding_criteria_satisfied(time, temperature, pressure, desired_state):
-    if desired_state == 'well-done' and time * temperature * pressure * COOKED_CONSTANT >= WELL_DONE: 
+    is_desired_state = desired_state == 'well-done' or 'medium'
+    is_criteria_satisfied_well_done = time * temperature * pressure * COOKED_CONSTANT >= WELL_DONE
+    is_criteria_satisfied_medium = time * temperature * pressure * COOKED_CONSTANT >= MEDIUM
+    
+    if desired_state == is_desired_state and is_criteria_satisfied_well_done: 
         return True
-    if desired_state == 'medium' and time * temperature * pressure * COOKED_CONSTANT >= MEDIUM:
+    if desired_state == is_desired_state and is_criteria_satisfied_medium:
         return True
     return False
