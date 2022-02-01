ffcc77aeb4f302c58f76e52c4fa0186a512e87a1
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-02-01 12:44:52 -0800

    exercise3

--- get_price.py
+++ get_price.py
@@ -1,10 +1,11 @@
 # Adapted from a Java code in the "Refactoring" book by Martin Fowler.
 
 # TODO: Replace temporary variable with extracted method/query
-
+def getBasePrice():
+    return quantity * item_price
 # Code snippet. Not runnable.
 def get_price():
-    base_price = quantity * item_price
+    base_price = getBasePrice()
     discount_factor = 0
     if base_price > 1000:
         discount_factor = 0.95
