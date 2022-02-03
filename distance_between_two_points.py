e99dbfb66bb5ced9b8831b7940c1c0835e803e99
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-02-03 13:37:35 -0800

    challenge5

--- distance_between_two_points.py
+++ distance_between_two_points.py
@@ -2,13 +2,17 @@
 # Example for Compose Methods: Extract Method.
 
 import math
+
+def distanceCalc(xc1, xc2, yc1, yc2):
+    return math.sqrt((xc1-xc2)**2 + (yc1 - yc2)**2)
+
 xc1 = 4
 yc1 = 4.25
 
 xc2 = 53
 yc2 = -5.35
 # Calculate the distance between the two circle
-distance = math.sqrt((xc1-xc2)**2 + (yc1 - yc2)**2)
+distance = distanceCalc(xc1, xc2, yc1, yc2)
 print('distance', distance)
 # *** somewhere else in your program ***
 xa = -36
@@ -17,5 +21,5 @@ ya = 97
 xb = .34
 yb = .91
 # calcualte the length of vector AB vector which is a vector between A and B points.
-length = math.sqrt((xa-xb)*(xa-xb) + (ya-yb)*(ya-yb))
+length = distanceCalc(xa, xb, ya, yb)
 print('length', length)
